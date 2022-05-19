22ec9e434345ba146491be2c3eed31919a6a6c78
Anay Ghatpande <anay.ghatpande@tu-dortmund.de>
2022-05-19 14:05:57 +0200

    Training script main

--- train.py
+++ train.py
@@ -2,37 +2,59 @@ import os
 import open3d.ml as _ml3d
 import open3d.ml.torch as ml3d
 
-cfg_file = "/home/iiwa-2/Frameworks/Open3D-ML/ml3d/configs/kpconv_s3dis.yml"
+#Reading the Dataset
+
+# dataset = ml3d.datasets.HopeDataset(dataset_path = "/home/iiwa-2/Downloads/Datasets/HOPE_demo/", name="HopeDataset")
+# #dataset = ml3d.datasets.HopeDataset(dataset_path='/home/iiwa-2/Downloads/Datasets/HOPE')
+#
+# # get the 'all' split that combines training, validation and test set
+# all_split = dataset.get_split('all')
+#
+# # print the attributes of the first datum
+# print(all_split.get_attr(0))
+#
+# # print the shape of the first point cloud
+# print(all_split.get_data(0)['point'].shape)
+#
+# # show the first 100 frames using the visualizer
+# vis = ml3d.vis.Visualizer()
+# vis.visualize_dataset(dataset, 'all', indices=range(5))
+
+#configuration
+from open3d._ml3d.torch import KPFCNN, SemanticSegmentation
+
+framework = "torch" # or tf
+cfg_file = "/home/iiwa-2/Frameworks/Open3D-ML/ml3d/configs/kpvconv_HOPE.yml"
 cfg = _ml3d.utils.Config.load_from_file(cfg_file)
-dataset_name = 'HOPE'
+dataset_name = 'HopeDataset'
 model = ml3d.models.KPFCNN(**cfg.model)
-cfg.dataset['dataset_path'] = "/home/iiwa-2/Downloads/Datasets/HOPE"
-dataset = ml3d.datasets.S3DIS(cfg.dataset.pop('dataset_path', None), **cfg.dataset)
-#pipeline = ml3d.pipelines.SemanticSegmentation(model, dataset=dataset, device="gpu", **cfg.pipeline, split="train")
+dataset_path = "/home/iiwa-2/Downloads/Datasets/HOPE/"
+dataset = ml3d.datasets.HopeDataset(dataset_path=dataset_path, name='HopeDataset', train_dir='train', test_dir='test', val_dir='val', use_cache=True)
+
+#dataset = ml3d.datasets.(cfg.dataset.pop('dataset_path', None), **cfg.dataset)
+pipeline = ml3d.pipelines.SemanticSegmentation(model, dataset=dataset, device="gpu", **cfg.pipeline, split="train")
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.dirname("/home/iiwa-2/Downloads/Datasets/HOPE")
-print(ROOT_DIR)
-data_dir = os.path.join(ROOT_DIR, dataset_name)
-train_data_dir = os.path.join(data_dir, 'train')
-print(train_data_dir)
-# # download the weights.
-# ckpt_folder = "./logs/"
-# os.makedirs(ckpt_folder, exist_ok=True)
-# ckpt_path = ckpt_folder + "randlanet_semantickitti_202201071330utc.pth"
-# randlanet_url = "https://storage.googleapis.com/open3d-releases/model-zoo/randlanet_semantickitti_202201071330utc.pth"
-# if not os.path.exists(ckpt_path):
-#     cmd = "wget {} -O {}".format(randlanet_url, ckpt_path)
-#     os.system(cmd)
-#
-# # load the parameters.
-# pipeline.load_ckpt(ckpt_path=ckpt_path)
-#
-# test_split = dataset.get_split("test")
-# data = test_split.get_data(0)
-#
-# # run inference on a single example.
-# # returns dict with 'predict_labels' and 'predict_scores'.
-# result = pipeline.run_inference(data)
-#
-# # evaluate performance on the test set; this will write logs to './logs'.
-# pipeline.run_test()
+# ROOT_DIR = os.path.dirname("/home/iiwa-2/Downloads/Datasets/HOPE")
+# print(ROOT_DIR)
+# data_dir = os.path.join(ROOT_DIR, dataset_name)
+# train_data_dir = os.path.join(data_dir, 'train')
+# print(train_data_dir)
+
+# fetch the classes by the name
+Pipeline = _ml3d.utils.get_module("pipeline", cfg.pipeline.name, framework)
+Model = _ml3d.utils.get_module("model", cfg.model.name, framework)
+Dataset = _ml3d.utils.get_module("dataset", cfg.dataset.name)
+
+# training own model
+
+# use a cache for storing the results of the preprocessing (default path is './logs/cache')
+#dataset = ml3d.datasets.HopeDataset(dataset_path='/home/iiwa-2/Downloads/Datasets/HOPE', use_cache=True, name="HopeDataset")
+
+# create the model with initialization.
+model = KPFCNN()
+
+pipeline = SemanticSegmentation(model=model, dataset=dataset, max_epoch=100, split='train')
+
+# prints training progress in the console.
+pipeline.run_train()
+
