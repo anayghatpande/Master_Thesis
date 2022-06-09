101ef9a081f87792474a5a8cd2736508b03dbea2
Anay Ghatpande <anay.ghatpande@tu-dortmund.de>
2022-06-09 19:37:59 +0200

    changes for YCB dataset

--- Point_cloud_converter.py
+++ Point_cloud_converter.py
@@ -8,18 +8,24 @@ import os
 import shutil
 from pyntcloud import PyntCloud
 import pandas as pd
+from multiprocessing import Process
+
 dataset = input("Choose Dataset or enter path:")
 if dataset == str(1):
     dataset_path = "/home/iiwa-2/Downloads/Datasets/hope_val/val/"
     model_path = sorted(glob.glob("/home/iiwa-2/Downloads/hope_models/models/*ply"))
+    output_dataset = "/home/iiwa-2/Downloads/Datasets/HOPE_S3ID/open3D/data/s3dis/FLW_dataset/"
     print("Selected Dataset is: HOPE")
 elif dataset == str(2):
     dataset_path = "/home/iiwa-2/Downloads/Datasets/ycbv/train_pbr/"
     model_path = sorted(glob.glob("/home/iiwa-2/Downloads/ycbv_models/models/*ply"))
+    output_dataset = "/media/iiwa-2/MEDIA/YCB_S3DIS/open3D/data/s3dis/FLW_dataset/"
     print("Selected Dataset is: YCB-V")
 else:
     dataset_path = dataset
+    output_dataset = input("Choose Dataset or enter path:")
     model_path = input("enter models path:")
+
 
 
 #dataset_path = "/home/iiwa-2/Downloads/hope_val/val/000001"
@@ -58,7 +64,7 @@ def depth_reader(d):
 def pcd_writer(path):
     # removing all old pcd images
     shutil.rmtree(path, ignore_errors=True)
-    os.mkdir(path)
+    os.makedirs(path)
     return path
 
 def bin_remover(path):
@@ -70,20 +76,21 @@ def bin_remover(path):
 dataset = list(dataset_read)
 config = load_json(dataset_path)
 
-
+#print(depth_images, len(depth_images))
 #@jit(nopython=True)
 def converter():
     for i in range(len(dataset)):
+        #shutil.rmtree(output_dataset + str(i), ignore_errors=True)
         rgb_images = rgb_reader(dataset[i])
         depth_images = depth_reader(dataset[i])
-        pcd_path = str(dataset[i]) + "/pcd"
-        npy_path = str(dataset[i]) + "/npy"
-        bin_path = str(dataset[i]) + "/bin"
-        anno_path = str(dataset[i]) + "/annotations"
+        pcd_path = str(output_dataset) + str(i) + "/pcd/"
+        npy_path = str(output_dataset) + str(i) + "/"
+        #bin_path = str(dataset[i]) + "/bin"
+        #anno_path = str(dataset[i]) + "/annotations"
         pcd_img = pcd_writer(pcd_path)
-        npy_files = pcd_writer(npy_path)
-        bin_files = pcd_writer(bin_path)
-        annotations = pcd_writer(anno_path)
+        #npy_files = pcd_writer(npy_path)
+        #bin_files = pcd_writer(bin_path)
+        #annotations = pcd_writer(anno_path)
 
         for j in range(len(depth_images)):
             color_raw = cv2.imread(rgb_images[j])
@@ -99,8 +106,23 @@ def converter():
             pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, config[2])
             pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
             #o3d.visualization.draw_geometries([pcd]) #debug
+            txt_path = npy_path + str(j) + "/"
 
-            o3d.io.write_point_cloud(str(pcd_img) + "/" + str(j) + ".ply", pcd)
+            ply_file_path = str(pcd_img) + "/" + str(j) + ".ply"
+            if not os.path.exists(ply_file_path):
+                o3d.io.write_point_cloud(ply_file_path, pcd)
+                #os.mkdir(txt_path)
+                #print("Directory ", pcd_img,  " Created ")
+            else:
+                print("Directory ", ply_file_path,  " already exists... skipping to next")
+
+            if not os.path.exists(txt_path):
+                os.mkdir(txt_path)
+                #print("Directory ", pcd_img,  " Created ")
+            else:
+                print("Directory ", txt_path,  " already exists... skipping to next")
+
+
             #print(pd.DataFrame(pcd.points))
             cloud_new = PyntCloud.from_file(str(pcd_img) + "/" + str(j) + ".ply")
             #cloud_2 = PyntCloud(pd.DataFrame(cloud_new))
@@ -108,9 +130,10 @@ def converter():
             array2 = np.asarray(pcd.colors)
             #print(array2)
             df = pd.DataFrame(cloud_new.points)
-            print(df[['x', 'y', 'z', 'red', 'green', 'blue']])
+            #print(df[['x', 'y', 'z', 'red', 'green', 'blue']])
 
-            df.to_csv(str(npy_files) + "/" + str(j) + ".txt", index=False, header=False, sep=' ')
+            #df.to_csv(txt_path + str(j) + ".txt", index=False, header=False, sep=' ')
+
             #with open(str(npy_files) + "/" + str(j) + ".txt", 'w') as f:
             #    dfstring = df.to_string(header=False, index=False)
             #    f.write(dfstring)
@@ -122,7 +145,7 @@ def converter():
                             # sep=" ",
                             # header=0,
                             # points=["x","y","z"])
-            cloud_new.to_file(str(bin_files) + "/" + str(j) + ".bin")
+            #cloud_new.to_file(str(bin_files) + "/" + str(j) + ".bin")
             #
             # #debugging
             # npzfile = np.load(str(npy_files) + "/" + str(j) + ".npz")
@@ -141,10 +164,25 @@ def converter():
 
         print("writing point-clouds in scene", i)
 
-
-
     print("Conversion Successful")
 
+# def info(title):
+#     print(title)
+#     print('module name:', __name__)
+#     print('parent process:', os.getppid())
+#     print('process id:', os.getpid())
+#
+# def f(name):
+#     #info('function f')
+#     print('hello', name)
+#     converter()
+#
+#
+# if __name__ == '__main__':
+#     info('main line')
+#     p = Process(target=f, args=('process is Running',))
+#     p.start()
+#     p.join()
 
 
 if __name__ == "__main__":
